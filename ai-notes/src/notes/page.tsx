7dc8d08179d9bdf851a1334bddaaf8feb94621c0
riya1807pro <riyakaushik6410@gmail.com>
2025-02-23 08:42:35 +0000

    remove extra pakages

--- ai-notes/src/notes/page.tsx
+++ ai-notes/src/notes/page.tsx
@@ -1,9 +1,18 @@
+import prisma from "@/lib/db/prisma";
+import { auth } from "@clerk/nextjs/server"
 import {Metadata} from "next"
 
 export const metadata : Metadata = {
     title : "FlowBraim - notes"
 }
 
-export const NotesPage =() =>{
-    return <div>her will be your notes</div>
+export default async function notesPage() {
+    const {userId} = await auth()
+
+    if(!userId) throw Error ("userId not found ");
+
+
+    const allNotes  = await prisma.note.findMany({where:{userId}})
+
+    return <div>{JSON.stringify(allNotes)}</div>
 }
